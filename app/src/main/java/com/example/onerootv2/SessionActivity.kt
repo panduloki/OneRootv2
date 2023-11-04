c5584105d641fb9eb8747e85f7102b1a8ed0fde1
sai mahesh <gmaheshpandu123@gmail.com>
2023-11-04 18:20:48 +0530

    changes in sessionActivity: created previous and next button to see images stored in mobile, connected with google cloud services and uploaded images successfully with progressbar.

--- app/src/main/java/com/example/onerootv2/SessionActivity.kt
+++ app/src/main/java/com/example/onerootv2/SessionActivity.kt
@@ -1,10 +1,16 @@
 package com.example.onerootv2
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import com.google.firebase.storage.ktx.storageMetadata
 import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
@@ -16,11 +22,143 @@ import java.io.IOException
 
 
 class SessionActivity : AppCompatActivity() {
+    private lateinit var sessionView: ImageView
+    private lateinit var previousButton: Button
+    private lateinit var nextButton: Button
+    private var id2 = 0
+    private var noOfFiles = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         readSessionFromStorage()
         setContentView(R.layout.activity_session)
+        sessionView = findViewById(R.id.sessionImageView)
+
+        previousButton = findViewById(R.id.previousButton)
+
+        nextButton = findViewById(R.id.nextButton)
+
+        val gpath: String = Environment.getExternalStorageDirectory().absolutePath
+        val spath = "DCIM/one_root_images/session1/"
+        val fullPath = File(gpath + File.separator + spath+ File.separator)
+
+        val files = fullPath.listFiles()
+        val imagePaths = mutableListOf<String>()
+        for (file in files!!) {
+            if (file.name.endsWith(".jpg")) {
+                imagePaths.add(file.path)
+            }
+        }
+
+        //imageReaderNew(fullPath)
+        println("full path: $fullPath")
+        println("imagePaths: $imagePaths")
+
+        noOfFiles = imagePaths.size
+
+
+
+        previousButton.setOnClickListener{
+            id2-=1
+            println("previous button clicked")
+
+            if (id2<=0){
+                id2 = 0
+            }
+            if (noOfFiles>1)
+            {
+                if (id2>=noOfFiles-1){
+                    id2 = noOfFiles-1
+                }
+            }
+            else{
+                println("error :failed to list files")
+            }
+
+
+            val image_path1 = imagePaths[id2]
+            println("image loading from $image_path1")
+            val uri = Uri.fromFile(File(image_path1))
+            // Set the image on the ImageView.
+            sessionView.setImageURI(uri)
+            sendImageToGoogleCloud(uri)
+        }
+
+
+        nextButton.setOnClickListener{
+            id2+=1
+            println("next button clicked")
+
+            if (id2<=0){
+                id2 = 0
+            }
+            if (noOfFiles>1)
+            {
+                if (id2>=noOfFiles-1){
+                    id2 = noOfFiles-1
+                }
+            }
+            else{
+                println("error :failed to list files")
+            }
+
+
+            val image_path1 = imagePaths[id2]
+            println("image loading from $image_path1")
+            val uri = Uri.fromFile(File(image_path1))
+            // Set the image on the ImageView.
+            sessionView.setImageURI(uri)
+            sendImageToGoogleCloud(uri)
+        }
+
+
     }
+
+
+    private fun sendImageToGoogleCloud(uri1: Uri){
+        // start
+        // https://firebase.google.com/docs/storage/android/start
+        // storage
+        // https://firebase.google.com/docs/storage/android/upload-files
+        // google cloud setup
+        // https://firebase.google.com/docs/android/setup
+
+        // error in bytes -> https://gorkemkara.medium.com/upload-files-to-android-firebase-stroage-4228fdd8d47f
+        val storage = Firebase.storage
+
+        // Create a storage reference from our app
+        val storageRef = storage.reference
+
+        // File or Blob
+        //val file = Uri.fromFile(File("path/to/mountains.jpg"))
+
+        // Create the file metadata
+        val metadata = storageMetadata {
+            contentType = "image/jpeg"
+        }
+
+        // Upload file and metadata to the path 'images/mountains.jpg'
+        val uploadTask = storageRef.child("images/${uri1.lastPathSegment}").putFile(uri1, metadata)
+
+        // Listen for state changes, errors, and completion of the upload.
+        // You'll need to import com.google.firebase.storage.component1 and
+        // com.google.firebase.storage.component2
+        uploadTask.addOnProgressListener {
+            val progress = (100.0 * it.bytesTransferred) / it.totalByteCount
+            println("Upload is $progress% done")
+        }.addOnPausedListener {
+            println("Upload is paused")
+        }.addOnFailureListener {
+            // Handle unsuccessful uploads
+            println("error: uploading image to goggle cloud")
+        }.addOnSuccessListener {
+            // Handle successful uploads on complete
+            // ...
+            println("image uploaded to google successfully")
+        }
+
+    }
+
 
     private fun readSessionFromStorage()
     {
@@ -54,7 +192,6 @@ class SessionActivity : AppCompatActivity() {
             }
             return stringBuilder.toString()
         }
-
 
         // read file  from external storage
         try {
@@ -98,7 +235,6 @@ class SessionActivity : AppCompatActivity() {
             println("session data read from folder successfully")
         }
     }
-
 
     @Deprecated("Deprecated in Java")
     override fun onBackPressed() {
